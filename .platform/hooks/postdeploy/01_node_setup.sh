c217257713828b49551378633034ddcf8485df14
nicholas <nicholas@boostorder.com>
2023-04-13 19:28:20 +0800

    Move node setup to post deployment

---
+++ .platform/hooks/postdeploy/01_node_setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /var/app/current
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash
+
+# Source nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Install node
+nvm install 16
+
+npm i && npm run build
