d23d2c1d8f36c38b89fee3260dc0d2f49d04998b
nicholas <nicholas@boostorder.com>
2023-04-14 01:15:35 +0800

    Use yum instead of nvm

--- .platform/hooks/postdeploy/01_node_setup.sh
+++ .platform/hooks/postdeploy/01_node_setup.sh
@@ -4,16 +4,13 @@ cd /var/app/current
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash
 
-# Source nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+sudo yum remove -y nodejs npm
 
-# Install node
-nvm install 16
+sudo rm -fr /var/cache/yum/*
 
-nvm alias default 16
+sudo yum clean all
 
-ln -sf  $(nvm which 16) /usr/bin/node
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
 
+sudo yum install nodejs -y
 npm i && npm run build
